161e2377a1a6d71f64b425a1657a6671678e680d
xsl <xsl@servyou.com.cn>
2023-05-23 14:13:34 +0800

    命令行参数读取

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+pub struct Config {
+    exe_path: String,
+    param1: String,
+    param2: Option<String>,
+    param3: Option<String>,
+}
+
+impl Config {
+    pub fn build(mut args : impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // 第一个参数是所在exe路径
+        let exe_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("exe_path获取失败"),
+        };
+        let param1 = match args.next() {
+            Some(arg) => arg,
+            None => return Err("param1获取失败"),
+        };
+        let param2 = args.next();
+        let param3 = args.next();
+        Ok(Config {
+            exe_path,
+            param1,
+            param2,
+            param3
+        })
+    }
+}
+
+pub fn run(config: Config) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use nm::{Config, run};
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(std::env::args()).unwrap_or_else(| err | {
+        eprintln!("{err}");
+        std::process::exit(1);
+    });
+    run(config);
 }
