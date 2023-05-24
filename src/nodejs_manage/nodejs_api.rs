08302440a34d3410ed575c4cb102e6cd552ee5f5
xsl <xiaosailun@163.com>
2023-05-24 23:31:34 +0800

    版本号解析

--- src/nodejs_manage/nodejs_api.rs
+++ src/nodejs_manage/nodejs_api.rs
@@ -10,21 +10,32 @@ struct Version_info {
 }
 
 // 版本号
+#[derive(Debug)]
 struct Version_number {
     one: String,
     two: String,
     three: String,
     version: String,
+    len: usize,
 }
 
 impl Version_number {
-    fn parse(version: String) -> Result<Version_number, String> {
+    fn parse<'a>(version: String) -> Result<Version_number, String> {
         let arr: Vec<_> = version.split('.').collect();
         if arr.len() > 3 {
             let str = format!("版本号：{version},格式不正确");
             return Err(str);
         }
-        Ok(Version_number { one: String::new(), two: String::new(), three: String::new(), version })
+        if arr[0] < "4" {
+            return Err("支持nodejs最低版本为4.0.0".into())
+        }
+        Ok(Version_number { 
+            one: arr[0].to_string(),
+            two: arr[1].to_string(),
+            three: arr[2].to_string(),
+            len: arr.len(),
+            version: version,
+        })
     }
 }
 
@@ -44,12 +55,18 @@ pub fn install(config: Config) -> Result<(), Box<dyn Error>> {
         Some(v) => v,
         None => return Err("版本号不存在".into())
     };
-    if version.as_str() < "4" {
-        return Err("支持nodejs最低版本为4.0.0".into())
-    }
     let version_number = Version_number::parse(version)?;
-
+    println!("{:?}",version_number);
     let version_infos = get_index_json()?;
+    // todo 版本号 1 2 3位分别匹配， 暂时先按整体来算了
+    let v = format!("v{}", version_number.version);
+    for version_info in version_infos {
+        if version_info.version.starts_with(&v) {
+            println!("{}", &version_number.version);
+            break;
+        }
+    }
+    // https://nodejs.org/dist/v9.11.2/node-v9.11.2-win-x64.zip
     Ok(())
 }
 
