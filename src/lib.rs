d825c13a0f60a4a7a4431b5f64e48a1882a1459f
xsl <xiaosailun@163.com>
2023-05-28 17:44:24 +0800

    warning修复

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ use std::{error::Error, path::Path, fs, io::Write};
 use nodejs_manage::nodejs_api;
 
 pub struct Config {
-    exe_path: String,
+    // exe_path: String,
     param1: String,
     param2: Option<String>,
-    param3: Option<String>,
+    // param3: Option<String>,
 }
 
 pub const CONFIG_PATH: &str = "./config.json";
@@ -17,7 +17,7 @@ pub const CONFIG_PATH: &str = "./config.json";
 impl Config {
     pub fn build(mut args : impl Iterator<Item = String>) -> Result<Config, &'static str> {
         // 第一个参数是所在exe路径
-        let exe_path = match args.next() {
+        let _exe_path = match args.next() {
             Some(arg) => arg,
             None => return Err("exe_path获取失败"),
         };
@@ -26,12 +26,12 @@ impl Config {
             None => return Err("param1获取失败"),
         };
         let param2 = args.next();
-        let param3 = args.next();
+        // let param3 = args.next();
         Ok(Config {
-            exe_path,
+            // exe_path,
             param1,
             param2,
-            param3
+            // param3
         })
     }
 }
